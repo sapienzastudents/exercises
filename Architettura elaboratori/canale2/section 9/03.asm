5bbfffd3537b7511cbf14d5ce6980898a03bc308
Alessio Giovannini <solifugo@protonmail.com>
2019-05-26 22:33:33 +0200

    third recursive assembly exercise

--- Architettura elaboratori/canale2/section 9/03.asm
+++ Architettura elaboratori/canale2/section 9/03.asm
@@ -19,8 +19,60 @@ main:
 
 	li $v0,5
 	syscall
-	move $x, $v0
+	move $y, $v0
 	
 	li $v0,5
 	syscall
-	move $x, $v0
+	move $z, $v0
+	
+	jal recursion
+	
+	lw $v0, ($sp)		# Recupero il valore finale
+	move $a0, $v0
+	li $v0, 1
+	syscall
+	
+	li $v0, 10
+	syscall						# Fine del programma
+	
+recursion:
+	subi $sp, $sp, 16
+	sw $ra, 0($sp)
+	sw $x, 4($sp)
+	sw $y, 8($sp)
+	sw $z, 12($sp)
+	
+	# Controlli per capire se ci troviamo nel caso base, cioe' se x*y*z = 0
+	mul $t0, $x, $y
+	mul $t0, $t0, $z
+	beqz $t0, cbase
+	
+	#Se non siamo nel caso base scambiamo le variabili e richiamiamo ricorsivamente la funzione
+	#x = z, y=x, z=y-1
+	move $t1, $x
+	move $x, $z
+	subi $z, $y,1
+	move $y, $t1
+	
+	jal recursion
+	
+	lw $v0, 0($sp)
+	lw $ra, 4($sp)
+	lw $x,  8($sp)
+	lw	 $y,  12($sp)
+	lw	 $z,  16($sp)
+	addi $sp, $sp, 20
+	
+	mul $t0, $x, $y
+	mul $t0, $t0, $z
+	mul $v0, $t0, $v0
+	subi $sp, $sp, 4
+	sw $v0, ($sp)
+	jr $ra
+
+cbase:
+	li $v0, 8						#		Valore da ritornare
+	addi $sp, $sp, 16
+	subi $sp, $sp, 4
+	sw $v0, 0($sp)				# Salva il valore che ritorna alla funzione precedente
+	jr $ra
