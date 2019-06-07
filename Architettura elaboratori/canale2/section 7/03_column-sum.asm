0660155428f163ed06056e9c3e20aebe8426bd71
Alessio Giovannini <solifugo@protonmail.com>
2019-06-07 20:35:11 +0200

    matrix asm upd

--- Architettura elaboratori/canale2/section 7/03_column-sum.asm
+++ Architettura elaboratori/canale2/section 7/03_column-sum.asm
@@ -4,8 +4,8 @@
 # Definita in memoria una matrice di 4 righe e 4 colonne con elementi byte, stampare in output 
 # la somma degli elementi prensenti lungo una colonna ESEMPIO:
 # MEMORIA
-# 3 6 7 8
-# 1 5 2 0
+# 3  6   7 8
+# 1  5   2 0
 # 6 8 10 5
 # 4 1 -9 2
 
@@ -13,12 +13,12 @@
 # COLONNA 1: 14
 # COLONNA 2: 20
 # COLONNA 3: 10
-# COLONNA 4: 25
+# COLONNA 4: 15
 
 .globl main 
 
 .data
-	M: .byte 3 6 7 8 1 5 2 0 6 8 10 5 4 1 -9 2
+	M: .byte 3, 6, 7, 8, 1, 5, 2, 0, 6, 8, 10, 5, 4, 1, -9, 2
 	R: .byte 4
 	C: .byte 4
 	coln: .asciiz "COLONNA "
@@ -37,56 +37,56 @@
 	syscall
 .end_macro
 
-.eqv $R, $t0 		# Numero di righe
-.eqv $C, $t1		# Numero di colonne
-.eqv $i, $t2		# Riga i-esima
-.eqv $j, $t3		# Colonna j-esima
-.eqv $idx, $t4		# Indice della matrice
-.eqv $el, $t5		# Elemento della matrice in esame
-.eqv $sum, $t6		# Somma colonna
+.eqv $R, $t0 						# Numero di righe
+.eqv $C, $t1							# Numero di colonne
+.eqv $i, $t2							# Riga i-esima
+.eqv $j, $t3							# Colonna j-esima
+.eqv $idx, $t4						# Indice della matrice
+.eqv $el, $t5						# Elemento della matrice in esame
+.eqv $sum, $t6					# Somma colonna
 		
 			
 .text
 main:
-	lb $C, C		# Carica valore del totale di colonne
-	lb $R, R		# Carica valore del totale di righe
-	li $i,1			# Inizializza riga
-	li $j,1			# Inizializza colonna
-	move $sum, $zero
+	lb $C, C							# Carica valore del totale di colonne
+	lb $R, R							# Carica valore del totale di righe
+	li $i,1								# Inizializza riga
+	li $j,1								# Inizializza colonna
+	move $sum, $zero			# Inizializza variabile di somma colonne
 	
 for:
-	bgt $j, $C, end
-	bgt $i, $R, reset
+	bgt $j, $C, end				# Se abbiamo terminato le colonne il processo è terminato
+	bgt $i, $R, reset				# Se abbiamo analizzato tutte le righe ricominciamo da capo con la prossima colonna
 
-	subi $idx, $i, 1			#
-	mul $idx, $i, $C			# Calcolo indice matrice
-	subi $t9, $j, 1				# C * (riga-1)+(colonna-1)
+	subi $idx, $i, 1					#
+	mul $idx, $idx, $C				# Calcolo indice matrice
+	subi $t9, $j, 1					# C * (riga-1)+(colonna-1)
 	add $idx, $idx, $t9			#
 	
 	lb $el, M($idx)
-	add $sum, $sum, $el			# Incremento della somma
-	addi $i, $i, 1				# Incremento della riga
+	add $sum, $sum, $el		# Incremento della somma
+	addi $i, $i, 1						# Incremento della riga
 	j for
 	
 reset:
-	la $a0, coln
-	li $v0, 4
-	syscall					# Stampa l'indicatore di colonna
-	move $a0, $j
-	li $v0, 1
-	syscall
+	la $a0, coln						#
+	li $v0, 4							#
+	syscall								# Stampa l'indicatore di colonna
+	move $a0, $j					#
+	li $v0, 1							#
+	syscall								#
 	
-	space					# Chiamata alla macro che stampa uno spazio
+	space								# Chiamata alla macro che stampa uno spazio
 	
 	move $a0, $sum
 	li $v0, 1
-	syscall					# Stampa del valore
+	syscall								# Stampa del valore
 	
-	carriage_return			# Chiamata alla macro che stampa l'andata a capo
+	carriage_return				# Chiamata alla macro che stampa l'andata a capo
 	
-	move $sum, $zero		# Resetta la somma delle colonne 
-	li $i, 1				# Resetta l'indice delle righe
-	addi $j, $j, 1			# Passa alla colonna successiva
+	move $sum, $zero			# Resetta la somma delle colonne 
+	li $i, 1								# Resetta l'indice delle righe
+	addi $j, $j, 1						# Passa alla colonna successiva
 	j for
 	
 end:
