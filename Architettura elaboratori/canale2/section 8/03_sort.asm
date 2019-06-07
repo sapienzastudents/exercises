c33d270f82126c788de1ed5c00ecdc47422f7849
solifugo <solifugo@protonmail.com>
2019-06-07 12:24:19 +0200

    sort.asm complete (section 8)

--- Architettura elaboratori/canale2/section 8/03_sort.asm
+++ Architettura elaboratori/canale2/section 8/03_sort.asm
@@ -1,5 +1,6 @@
 # http://arch2.000webhostapp.com/Esercizi.html <-- Sito degli esercizi
-# NON FUNZIONA	
+# Svolto da Alessio Giovannini
+# PS (ho utilizzato l'approccio Selection Sort per ordinare)	
 
 # Si scriva un programma in assembler che mediante la subroutine ORDINA ordina un vettore immesso da input. 
 # La routine ORDINA ha come argomento il vettore (cioe' l'indirizzo del vettore), e restituisce il vettore ordinato 
@@ -11,71 +12,73 @@
 .globl main
 
 .data	
-	inp: .word 0:6							# Inizializza un array di 6 elementi con valore 0 (vettore non ordinato)
-	out: .word 0:6							# Vettore di output ordinato	
-	
-.eqv $idx, $t0								# Indice (usato per il vettore in input)
-.eqv $start, $t1							# Indice (usato per il vettore di output)
-.eqv $max, $t2								# Numero elementi nei vettori
-.eqv $offs, $t3								# Usato per il calcolo dell'offset	
-.eqv $min, $t4								# Elemento minimo
+	inp: .word 0:6					# Inizializza un array di 6 elementi con valore 0
+
+.eqv $idx, $t0						# Indice primario
+.eqv $offs, $t1						# Offset primario
+.eqv $inn, $t2						# Indice secondario
+.eqv $pos, $t3						# Offset secondario
+.eqv $min, $t4						# Rappresenta il minimo
+.eqv $now, $t5						# Rappresenta elemento in esame del ciclo principale
+.eqv $curr, $t6						# Elemento corrente del ciclo innestato	
+
 
 .text
 main:
-	li $max, 6								# Capacita' massima del vettore
-	move $idx, $zero						# Inizializzazione dell'indice (vettore input)
-	move $start, $zero				   		# Inizializzazione dell'indice (vettore output)
-	li $min,  500							# Il numero piu' grande rappresentabile da una word
+	user_inp:
+	bge $idx, 6, ORDINA				# Condizione del ciclo
 	
-# Serve a  salvare il vettore messo in input
-save_vect:
-	bge $idx, $max, ORDINA
+	li $v0, 5			
+	syscall							# Leggo input da tastiera
 	
-	li $v0, 5
-	syscall									# Legge l'elemento
-	
-	sll $offs, $idx, 2 						# Calcolo dell'offset
-	sw $v0, inp($offs)						# Salvo l'elemento
-	addi $idx, $idx, 1						# Incremento del contatore
-	j save_vect
+	sll $offs, $idx, 2				# Calcolo dell'offset per il vettore
+	sw $v0, inp($offs)				# Salvataggio del valore letto nell'array
+	addi $idx, $idx, 1				# Incremento del contatore
+	j user_inp
 
-end: 
-# --------------------- START PRINT TEST ---------------------
-	move $start, $zero	
-print:
-	sll $offs, $start, 2
-	lw $a0, out($offs)
-	li $v0, 1
-	syscall
-	addi $start, $start, 1
-	blt $start, $max , print
-# ---------------------- END PRINT TEST ----------------------
-	la $v1, out								# indirizzo del vettore di output
-	li $v0, 10
-	syscall	
-			
 ORDINA:
-	bge $start, $max, end					# Se abbiamo finito di scorrere il vettore di output
-	move $idx, $start						# Copia l'indice
+	move $idx, $zero				# Resetta il contatore
+	move $inn, $idx					# Imposta il ciclo interno
+	
+	set:
+	bge $idx, 5, print				
+	
+	sll $offs, $idx, 2				# Calcolo l'offset del indice primario
+	lw $now, inp($offs)				# Elemento indice primario
+	move $min, $now					# Impostazione del minimo
+	addi $idx, $idx, 1				# Incrementa contatore primario
+	move $inn, $idx 				# Setto l'indice del ciclo innestato
+	
+	inner:
+		bge $inn, 6, insert
+		sll $t7, $inn, 2			# Calcolo offset ciclo interno 
+		lw $curr, inp($t7)			# Caricamento elemento
+		addi $inn, $inn, 1			# Incremento dell'indice del ciclo interno
+		bge $curr, $min, inner		# Se il numero corrente non è il nuovo minimo ricomincia il ciclo
+		
+		move $pos, $t7				# Ci salviamo l'offset
+		move $min, $curr			# l'elemento in esame è il nuovo minimo
+		j inner
+		
+	insert:
+		beq $now, $min, set			# Se l'elemento del primo ciclo è anche il minimo non fare nulla			
+		
+		sw $min, inp($offs)			# Sposta il minimo alla posizione esatta che verrà poi esclusa
+		sw $now, inp($pos)
+		
+		j set
 
-	jal find_min
-	
-	sll $offs, $start, 2					# Calcolo dell'offset per il vettore di output 
-	sw $min, out($offs)						# Salva il valore nella posizione corretta
-	addi $start, $start,1					# Incremento contatore
-	li $min, 500							# Resetta il minimo (con il numero piu' grande rappresentabile da una word)
-	j ORDINA
-	
-find_min:
-	bge $idx, $max, return
-	sll $offs, $idx, 2						# Calcolo dell'offset per il vettore di input
-	lw $t7, inp($offs)						# Caricamento elemento dall'array non ordinato
-	addi $idx, $idx,1
-	blt $t7, $min, swap						# Abbiamo trovato il nuovo minimo?
-	j find_min
-swap:
-	move $min, $t7
-	j find_min 
-return:
-	jr $ra
-	
+print:
+	move $idx, $zero				# Inizializzazione dell'indice
+	for:
+		sll $offs, $idx, 2	
+		lw $a0, inp($offs)
+		
+		li $v0, 1
+		syscall						# Stampo intero
+		
+		addi $idx, $idx, 1			# Incremento il contatore
+		blt $idx, 6, for
+		
+		li $v0, 10
+		syscall 
