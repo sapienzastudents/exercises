7c353663b490ee69f72b67d22700df8caac94402
Alessio Giovannini <solifugo@protonmail.com>
2019-06-09 16:29:37 +0200

    upd matrix asm

--- Architettura elaboratori/canale2/section 7/05_prod-matrix.asm
+++ Architettura elaboratori/canale2/section 7/05_prod-matrix.asm
@@ -1,5 +1,5 @@
 # http://arch2.000webhostapp.com/Esercizi.html <-- Sito degli esercizi
-# DA SVOLGERE
+# Svolto da Alessio Giovannini
 
 #Definita una matrice in memoria di 8 righe e 8 colonne con elementi halfword, A8x8, stampare in output una nuova matrice B8x4 in cui le colonne sono date 
 # dal prodotto degli elementi delle colonne della matrice originaria: 
@@ -23,9 +23,9 @@
 # B
 # 0008 0042 0000 0024
 # 0010 0080 0000 0010
-# 0000 0421 0001 0008
+# 0000 0420 0001 0008
 # 0044 0000 0000 0000
-# 1500 0340 0000 0052
+# 1500 0034 0000 0052
 # 0630 0064 0006 0015
 # 0020 0000 0000 0012
 # 0000 0028 0000 0000
@@ -35,37 +35,44 @@
 .data
 	M: .half 02 04 06 07 00 12 03 08 01 10 05 16 00 01 01 10 00 03 20 21 01 01 02 04 02 22 06 00 00 12 37 00 30 50 01 34 00 05 04 13 10 63 08 08 01 06 05 03 05 04 00 01 00 09 06 02 41 00 14 02 00 14 00 01
 	R: .word 8
-	C1: .word 8
-	C2: .word 4
+	C: .word 8
 
-.eqv $R, $t0
-.eqv $C1, $t1
-.eqv $C2, $t2
-.eqv $i, $t3
-.eqv $j, $t4
-.eqv $idx, $t5
+.eqv $R, $t0							# Numero delle righe
+.eqv $C, $t1							# Numero delle colonne
+.eqv $i, $t2							# Indice righe
+.eqv $j, $t3							# Indice colonne
+.eqv $idx, $t4							# Indice matrice
+
+.macro calc_idx
+#Calcolo indice matrice C * (r-1) + (c-1)
+	subi $idx, $i, 1
+	mul $idx, $idx, $C
+	subi $t9, $j, 1
+	add $idx, $idx, $t9
+	sll $idx, $idx, 1
+.end_macro
 
 .text
 main:
 	li $i, 1
 	li $j, 1
 	lw $R, R
-	lw $C1, C1
-	lw $C2, C2
+	lw $C, C
 	
 	for_riga:
 		bgt $i, $R, end
 		for_colonna:
-			bgt $j, $C1, new_row
+			bgt $j, $C, new_row
 			
-			#Calcolo indice matrice C * (r-1) + (c-1)
-			subi $idx, $i, 1
-			mul $idx, $idx, $C1
-			subi $t9, $j, 1
-			add $idx, $idx, $t9
-			sll $idx, $idx, 1
+			calc_idx						# Calcolo primo indice  (chiamata alla macro)
+			addi $j, $j, 1					# Incremento contatore
 			
-			lh $a0, M($idx)
+			lh $t8, M($idx)			# Caricamento elemento 
+			
+			calc_idx						# Calcolo del secondo indice (chiamata alla macro)
+			
+			lh $a0, M($idx)		
+			mul $a0, $a0, $t8		# calcolo del risultato
 			li $v0, 1
 			syscall
 			
@@ -73,7 +80,7 @@ main:
 			li $v0, 11
 			syscall
 			
-			addi $j, $j, 1
+			addi $j, $j, 1					# incremento del contatore
 			j for_colonna
 		
 		new_row:
