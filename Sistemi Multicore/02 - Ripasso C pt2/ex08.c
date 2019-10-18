159cf4f93379ff6de068013753742daf539a0fcc
Alessio Giovannini <thesolifugo@gmail.com>
2019-10-18 12:55:21 +0200

    ex08.c ripasso c complete

--- Sistemi Multicore/02 - Ripasso C pt2/ex08.c
+++ Sistemi Multicore/02 - Ripasso C pt2/ex08.c
@@ -12,3 +12,61 @@ Per fare l’esercizio si utilizzi l’allocazione dinamica.
 N.B. ricordarsi che le stringhe devono terminare sempre con il carattere
 terminatore stringa, ovvero ‘\0’
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char *leggiStringa(){
+    printf("Inserire la stringa: ");
+    char c,*str;
+    scanf("%c",&c);
+
+    str = (char*)malloc(sizeof(char));
+    if(str==NULL){
+        printf("Impossibile allocare memoria...");
+        return NULL;
+    }
+    
+    int i;
+    for (i=0;c!='\n';i++){
+        str[i]=c;
+        if(!(str = realloc(str, (i+2)*sizeof(char)))){
+            printf("Errore nell'allocazione di memoria\n");
+            return str;
+        };
+
+        scanf("%c",&c);
+    }
+    str[i]='\0';
+
+    return str;
+};
+
+void sostituisciNumeri(char *p){
+    int i=0;
+    while(p[i]!='\0'){
+         if (p[i]<='9' && p[i]>='0'){
+             p[i]='*';
+         }
+        i++;
+    }
+};
+
+int main(){
+    char *stringa;
+    
+    stringa = (char*)malloc(sizeof(char));
+    if (stringa==NULL){
+        printf("Impossibile allocare memoria\n");
+        return 1;
+    }
+
+
+    stringa = leggiStringa();
+    printf("Stringa originale -> %s\n",stringa);
+    
+    sostituisciNumeri(stringa);
+    printf("Stringa modificata -> %s\n",stringa);
+    
+    return 0;
+};
