747f4bdfb8e899f708112df16e90344d9cb8c629
Alessio Giovannini <thesolifugo@gmail.com>
2019-10-18 16:52:37 +0200

    prepared env for multithread exercise (Multicore)

---
+++ Sistemi Multicore/03 - Multithreading/ex01.c
@@ -0,0 +1,13 @@
+/*
+Problema del lettore-scrittore: un thread si occupa di scrivere la stringa ciaociao, 
+mentre altri tre si occupano della sua lettura. Non è possible avere letture 
+e scritture contemporaneamente. Risolvere il problema usando i mutex.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int main(){
+
+}
